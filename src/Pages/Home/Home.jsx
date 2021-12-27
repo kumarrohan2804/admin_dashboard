7478362899379393f9ae86d22cd1129a8eb887f0
Rohan Kumar <kumarrohan2804@gmail.com>
2021-12-28 00:01:01 +0530

    charts template implemented using recharts

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Chart from '../../Components/Chart/Chart';
 import FeaturedInfo from '../../Components/FeaturedInfo/FeaturedInfo';
 import "./home.css";
 
@@ -6,6 +7,7 @@ const Home = () => {
     return (
         <div className="home">
             <FeaturedInfo />
+            <Chart />
         </div>
     )
 }
