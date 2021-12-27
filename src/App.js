f08e4eb2be93dc48d71e844d09fc46d610c76596
Rohan Kumar <kumarrohan2804@gmail.com>
2021-12-27 23:03:09 +0530

    sidebar of the dashboard

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import Sidebar from './Components/Sidebar/Sidebar';
 import TopBar from './Components/TopBar/TopBar';
+import "./app.css";
 
 function App() {
   return (
     <div className="App">
       <TopBar />
+      <div className="container">
+        <Sidebar />
+        <div className="others">other pages</div>
+      </div>
     </div>
   );
 }
