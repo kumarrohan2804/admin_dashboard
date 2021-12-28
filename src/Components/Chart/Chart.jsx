e93264970ee4f9035db027fdeb5e5c5413b72c6d
Rohan Kumar <kumarrohan2804@gmail.com>
2021-12-28 21:53:38 +0530

    making a reusable component of chart

--- src/Components/Chart/Chart.jsx
+++ src/Components/Chart/Chart.jsx
@@ -3,79 +3,22 @@ import {
   LineChart,
   Line,
   XAxis,
-  YAxis,
   CartesianGrid,
   Tooltip,
-  Legend,
   ResponsiveContainer,
 } from "recharts";
 import "./chart.css";
 
-const Chart = () => {
-  const data = [
-    {
-      name: "Jan",
-      "Active User": 4000,
-      pv: 2400,
-      amt: 2400,
-    },
-    {
-      name: "Feb",
-      "Active User": 3000,
-      pv: 1398,
-      amt: 2210,
-    },
-    {
-      name: "Mar",
-      "Active User": 2000,
-    },
-    {
-      name: "Apr",
-      "Active User": 2780,
-    },
-    {
-      name: "May",
-      "Active User": 1890,
-    },
-    {
-      name: "Jun",
-      "Active User": 2390,
-    },
-    {
-      name: "Jul",
-      "Active User": 3490,
-    },
-    {
-      name: "Aug",
-      "Active User": 1490,
-    },
-    {
-      name: "Sep",
-      "Active User": 2490,
-    },
-    {
-      name: "Oct",
-      "Active User": 3090,
-    },
-    {
-      name: "Nov",
-      "Active User": 5490,
-    },
-    {
-      name: "Dec",
-      "Active User": 4490,
-    },
-  ];
-
+const Chart = ({ title, data, dataKey, grid }) => {
   return (
     <div className="chart">
-      <h3 className="chartTitle">Sales Analytics</h3>
+      <h3 className="chartTitle">{title}</h3>
       <ResponsiveContainer width="100%" aspect={4 / 1}>
         <LineChart data={data}>
           <XAxis dataKey="name" stroke="#5550bd" />
-          <Line type="monotone" dataKey="Active User" stroke="#5550bd" />
+          <Line type="monotone" dataKey={dataKey} stroke="#5550bd" />
           <Tooltip />
-          <CartesianGrid stroke="#e0dfdf" strokeDasharray="5 5" />
+          {grid && <CartesianGrid stroke="#e0dfdf" strokeDasharray="5 5" />}
         </LineChart>
       </ResponsiveContainer>
     </div>
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,15 +1,21 @@
-import React from 'react';
-import Chart from '../../Components/Chart/Chart';
-import FeaturedInfo from '../../Components/FeaturedInfo/FeaturedInfo';
+import React from "react";
+import Chart from "../../Components/Chart/Chart";
+import FeaturedInfo from "../../Components/FeaturedInfo/FeaturedInfo";
+import { userData } from "../../dummyData";
 import "./home.css";
 
 const Home = () => {
-    return (
-        <div className="home">
-            <FeaturedInfo />
-            <Chart />
-        </div>
-    )
-}
+  return (
+    <div className="home">
+      <FeaturedInfo />
+      <Chart
+        data={userData}
+        title="User Analytics"
+        grid
+        dataKey="Active User"
+      />
+    </div>
+  );
+};
 
-export default Home
+export default Home;
